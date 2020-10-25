5fab2ac1634ddce70641d6937300e4552442c9ab
Sylvain PHAM <sylvain@pham.es>
2020-10-25 14:28:00 +0100

    wip

--- tests/bowling-game.test.js
+++ tests/bowling-game.test.js
@@ -3,8 +3,20 @@ const BowlingGame = require('../src/bowling-game')
 describe('kata of game bowling', () => {
     it('should return 0 for a game of all zeros', async () => {
         const game = new BowlingGame();
-        game.roll(0);
+        for (let i = 0; i < 20; i++) {
+            game.roll(0);
+
+        }
         expect(game.score).toEqual(0)
+    });
+
+    it('should return 20 for a game of all one', async () => {
+        const game = new BowlingGame();
+        for (let i = 0; i < 20; i++) {
+            game.roll(1);
+
+        }
+        expect(game.score).toEqual(20)
     });
 
 });
