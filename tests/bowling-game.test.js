3c16132b8e4065b2c8fd76da9748c98744249fc1
Sylvain PHAM <sylvain@pham.es>
2020-10-25 15:27:53 +0100

    factorisation

--- tests/bowling-game.test.js
+++ tests/bowling-game.test.js
@@ -1,29 +1,38 @@
 const BowlingGame = require('../src/bowling-game')
 
+
+let game;
+
+beforeEach(() => {
+    game = new BowlingGame();
+})
+
 describe('kata of game bowling', () => {
-
-    let game;
-
-    beforeEach(() => {
-        game = new BowlingGame();
-    })
 
     it('should return 0 for a game of all zeros', async () => {
         //    const game = new BowlingGame();
-        for (let i = 0; i < 20; i++) {
-            game.roll(0);
+        // for (let i = 0; i < 20; i++) {
+        //     game.roll(0);
 
-        }
+        // }
+        rollMany(0, 20);
         expect(game.score).toEqual(0)
     });
 
     it('should return 20 for a game of all one', async () => {
         //  const game = new BowlingGame();
-        for (let i = 0; i < 20; i++) {
-            game.roll(1);
+        // for (let i = 0; i < 20; i++) {
+        //     game.roll(1);
 
-        }
+        // }
+        rollMany(1, 20);
         expect(game.score).toEqual(20)
     });
 
 });
+
+function rollMany(pins, rolls) {
+    for (let i = 0; i < rolls; i++) {
+        game.roll(pins)
+    }
+}
